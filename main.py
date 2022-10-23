476413604f200fe74d80bdc72efe265d924c7ccd
Mattia Cardillo <cardillomattia@yahoo.it>
2022-10-23 10:46:11 +0200

    Allow website to also function locally on my PC

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "sqlite:///blog.db")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
